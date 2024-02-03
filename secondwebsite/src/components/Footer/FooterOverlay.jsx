c98992ba498d97bcee24d61888fc92eec2035a3a
Mustafa Dzhindzhi <dzindzimustafa@gmail.com>
2024-02-03 16:20:46 +0200

    react

--- secondwebsite/src/components/Footer/FooterOverlay.jsx
+++ secondwebsite/src/components/Footer/FooterOverlay.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+
 import './FooterOverlay.css';
 
-const FooterOverlay = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const FooterOverlay = () => (
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
+  </div>
+);
 
-export default FooterOverlay
+export default FooterOverlay;
