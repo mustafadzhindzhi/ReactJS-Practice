edc721aa6b8edf0a950d97cd12c8f0160bc11b64
Mustafa Dzhindzhi <dzindzimustafa@gmail.com>
2024-02-06 00:01:52 +0200

    react

--- thirdwebsite/bank_modern_app/src/components/FeedBackCard.jsx
+++ thirdwebsite/bank_modern_app/src/components/FeedBackCard.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import { quotes } from "../assets";
 
-const FeedBackCard = () => {
-  return (
-    <div>
-      FeedBack Card
+const FeedBackCard = ({content, name, title, img}) => (
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+      <img src={quotes} alt="double_quotes" className="w-[42px] h-[27px] object-contain" />
+      
     </div>
   )
-}
 
 export default FeedBackCard
