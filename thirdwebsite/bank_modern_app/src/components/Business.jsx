6ab906a15f0632ade33ec0bfe4754754b6dc1979
Mustafa Dzhindzhi <dzindzimustafa@gmail.com>
2024-02-05 21:43:26 +0200

    react

--- thirdwebsite/bank_modern_app/src/components/Business.jsx
+++ thirdwebsite/bank_modern_app/src/components/Business.jsx
@@ -2,11 +2,30 @@ import { features } from "../constants";
 import styles, {layout} from "../style.js";
 import Button from './Button.jsx';
 
+const FeatureCard = ({icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "bm-0"} feature-card`}>
+    <div>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
-    <div>
-      Business
-    </div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden"/> we'll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on market.</p>
+
+        <Button styles="mt-10"/>
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature}/>
+        ))}
+      </div>
+    </section>
   )
 }
 
